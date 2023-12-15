ba6f1215905fe85cb9428dfa8b85aec5718d80a3
maugarciav <mau.villanueva2002@gmail.com>
2023-12-15 12:47:31 -0600

    day2: mapping users

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,19 +1,39 @@
 import './App.css'
 import { TwitterFollowCard } from "./TwiiterFollowCard"
 
+const users = [
+    {
+        userName: 'maugarciav',
+        name: 'mau garcia',
+        isFollowing: true
+    },
+    {
+        userName: 'twitter',
+        name: 'Twitter',
+        isFollowing: false
+    },
+    {
+        userName: 'midudev',
+        name: 'Miguel Angel'
+    },
+]
+
 export function App (){
     return(
         <div className="App">
-            <TwitterFollowCard 
-                userName="maugarciav">
-                <strong>Mau Garcia</strong>
-            </TwitterFollowCard>
-
-            <TwitterFollowCard 
-                userName="pherab" >
-                <strong>Pablito Gamer</strong>
-            </TwitterFollowCard>
-            
+            {
+                users.map(({userName, name, isFollowing}) => {
+                    return(
+                        <TwitterFollowCard
+                            key={userName}
+                            userName={userName}
+                            initalIsFollowing = {isFollowing}
+                        >
+                            <strong>{name}</strong>
+                        </TwitterFollowCard>
+                    )
+                })
+            }
         </div>
     )
 }
--- projects/00-hola-mundo/src/TwiiterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwiiterFollowCard.jsx
@@ -2,19 +2,18 @@ import './App.css'
 import { useState } from 'react'
 
 
-export function TwitterFollowCard ({children, userName}){
-    const [isFollowing, setIsFollowing] = useState(false)
+export function TwitterFollowCard ({children, userName, initalIsFollowing}){
+    const [isFollowing, setIsFollowing] = useState(initalIsFollowing)
 
-    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+    const text = isFollowing ? 'Following' : 'Follow'
     const buttonClassName = isFollowing 
         ? 'tw-followCard-button is-following'
         : 'tw-followCard-button'
-
     
     const handleClick = () => {
         setIsFollowing(!isFollowing)
     }
-    
+
     return(
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
@@ -31,7 +30,8 @@ export function TwitterFollowCard ({children, userName}){
             </header>
             <aside>
                 <button className={buttonClassName} onClick={handleClick}>
-                    {text} 
+                    <span className='tw-followCard-text'> {text} </span>
+                    <span className='tw-followCard-stopFollow'>Unfollow</span>
                 </button>
             </aside>
         </article>
