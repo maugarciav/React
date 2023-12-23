129687c953d7824321e180de773aa81a25e9eb4d
maugarciav <mau.villanueva2002@gmail.com>
2023-12-22 22:18:21 -0600

    feat: winners and more game logic

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -21,17 +21,58 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   );
 };
 
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+]
+
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    //Check for winners
+    for(const combo of WINNER_COMBOS){
+      const [a,b,c] = combo
+      if(
+        boardToCheck[a] &&
+        boardToCheck[b] === boardToCheck[a] &&
+        boardToCheck[c] === boardToCheck[a]
+      ){
+        return boardToCheck[a]
+      }
+    }
+    // No winner
+    return null
+  }
 
   const updateBoard = (index) => {
-    const newBoard = [...board]
+    // Can't change an already selected space
+    if(board[index] || winner) return
+
+    
+    // Update board
+    const newBoard =  [...board]
     newBoard[index] = turn
     setBoard(newBoard)
 
+    // Update turn
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    //Look for winner
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){
+      setWinner(newWinner)
+    }
   };
 
   return (
@@ -55,5 +96,3 @@ function App() {
 }
 
 export default App;
-
-//23:07
