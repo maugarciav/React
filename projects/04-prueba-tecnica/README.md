11b5a619158c97160c7956d6d804d2ca8a42b33c
maugarciav <mau.villanueva2002@gmail.com>
2024-01-04 20:25:14 -0600

    feat: fetch data

--- projects/04-prueba-tecnica/README.md
+++ projects/04-prueba-tecnica/README.md
@@ -4,6 +4,7 @@ APIs:
 
 - Facts Random: https://catfact.ninja/fact
 - Imagen random: https://cataas.com/cat/says/hello
+    -ENPOINT: `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
 
 - Recupera un hecho aleatorio de gatos de la primera API
 - Recuperar la primera palabra del hecho
--- projects/04-prueba-tecnica/src/App.jsx
+++ projects/04-prueba-tecnica/src/App.jsx
@@ -1,7 +1,22 @@
-export function App () {
+import { useEffect, useState } from 'react';
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+//const CAT_ENPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+
+export function App() {
+  const [fact, setFact] = useState()
+
+  //Pensando que no se puede usar axios.
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => setFact(data.fact))
+  },[])
+
   return (
-    <h1>
-      App 
-    </h1>
-  )
+    <main>
+      <h1>App</h1>
+      {fact && <p>{fact}</p>}
+    </main>
+  );
 }
