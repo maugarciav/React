e1236df69aa3bf9bf9576ac37620c060d8713c23
maugarciav <mau.villanueva2002@gmail.com>
2024-01-06 19:56:00 -0600

    feat: fetch info and get img, add css

--- projects/04-prueba-tecnica/src/App.jsx
+++ projects/04-prueba-tecnica/src/App.jsx
@@ -1,22 +1,35 @@
 import { useEffect, useState } from 'react';
+import './App.css'
+
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
-//const CAT_ENPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
 
 export function App() {
   const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState(null)
+
 
   //Pensando que no se puede usar axios.
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(res => res.json())
-      .then(data => setFact(data.fact))
+      .then(data => {
+        const {fact} = data
+        setFact(fact)
+
+        const firstWord = fact.split(' ', 3).join(' ')
+
+        setImageUrl(`https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=white`)
+      })
   },[])
 
   return (
     <main>
       <h1>App</h1>
+      <section>
       {fact && <p>{fact}</p>}
+      {imageUrl && <img src={imageUrl} alt={`Image from first word of ${fact}`} />}
+      </section>
     </main>
   );
 }
